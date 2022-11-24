e50a3c6a180df7f4fadc1933a34bd17201bd7ba0
Anthony Mogotlane <almogotlane@gmail.com>
2022-11-24 06:44:07 +0200

    Added http delete method

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using FruitTracker.Model;
 using FruitTracker.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -11,23 +12,21 @@ app.MapGet("/api/fruits", () => {
     return  Results.Ok(fruitDb.GetFruits());
 });
 
-// app.MapGet("/api/fruits/{id}", (int id) => {
-//     return Results.Ok(fruitDb.GetFruits().Find(o => o.Id == id));
-// });
-
-app.MapPost("/api/fruits", (Fruit input) => {
-
-    var fruit = fruitDb.GetFruits().Find(fruit => fruit.Name == input.Name);
-
-    if (fruit == null) {
-        fruitDb.AddFruit(input.Name!);
-    } else {
-        fruit.Count++;
-    }
-
-    return Results.Created($"api/fruits/{input.Id}", input);
+app.MapGet("/api/fruits/{name}", (string name) => {
+    return Results.Ok(fruitDb.GetName(name));
 });
 
+app.MapPost("/api/fruits", (Fruit fruit) => {
+    
+    fruitDb.AddFruit(fruit.Name!);
+
+    return Results.Created($"api/fruits/{fruit.Name}", fruit);
+});
+
+app.MapDelete("/api/fruits/{name}", (string name) => {
+    fruitDb.RemoveName(name);
+    return Results.Ok();
+});
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
